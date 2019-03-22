f7105c4522aad10040f128fe10eeda2e76f8ea3d
teachgame <teachgame@163.com>
2019-03-22 18:15:22 +0800

    Testing

---
+++ TestTK/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace LearnOpenGL_TK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game window = new Game(800, 600, "Test TK");
+            Console.WriteLine("Let's do this.");
+            window.Run(60.0);
+            Console.WriteLine("Good Bye");
+            Console.ReadKey();
+        }
+    }
+}
